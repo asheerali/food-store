1ca9c081cacf1e44388204a720ca80811b196a90
asheerali <asheerali1997@gmail.com>
2024-09-08 20:08:29 +0200

    first commit

---
+++ src/componenets/Header.jsx
@@ -0,0 +1,15 @@
+const Header = () => {
+  return (
+    <div>
+      <img src="" alt="" />
+      <h1></h1>
+      <div>
+        <nav>
+          <button> Cart (0)</button>
+        </nav>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
